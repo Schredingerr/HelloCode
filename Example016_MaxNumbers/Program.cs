df3b75d1a9d5214758a92fc59362dcf56d600a77
Schredinger <gameboxofschredinger@gmail.com>
2021-11-27 13:38:13 +0300

    Add Example 016 Max numbers

---
+++ Example016_MaxNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = new Random().Next(1, 100); //1 2 3 4 ...
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 100);
+Console.WriteLine(numberC);
+int max = numberA;
+if(numberA > max) max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+Console.Write("max = ");
+Console.WriteLine(max);
