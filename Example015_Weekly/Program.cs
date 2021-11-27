63d3088ddd86e0b1bdd3ded67f00417502c9731f
Schredinger <gameboxofschredinger@gmail.com>
2021-11-27 13:15:41 +0300

    add Example 015 Weekly

---
+++ Example015_Weekly/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("ВВедите номер дня недели: ");
+string weekly = Console.ReadLine();
+
+if(weekly.ToLower() == "1")
+{
+    Console.WriteLine("Today is Monday");
+}
+
+if(weekly.ToLower() == "2")
+{
+    Console.WriteLine("Today is Tuesday");
+}
+
+if(weekly.ToLower() == "3")
+{
+    Console.WriteLine("Today is Wednesday");
+}
+
+if(weekly.ToLower() == "4")
+{
+    Console.WriteLine("Today is Thursday");
+}
+
+if(weekly.ToLower() == "5")
+{
+    Console.WriteLine("Today is Friday");
+}
+
+if(weekly.ToLower() == "6")
+{
+    Console.WriteLine("Today is Saturday");
+}
+
+if(weekly.ToLower() == "7")
+{
+    Console.WriteLine("Today is Sunday");
+}
