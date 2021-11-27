e042e7d6d563105d3201d80cdfb1cb5d5ffa8c7d
Schredinger <gameboxofschredinger@gmail.com>
2021-11-27 10:35:39 +0300

    add Greater&Lesser

---
+++ Example014_Greater&Lesser/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random().Next(1, 100); //1 2 3 4 ...
+Console.WriteLine(numberA);
+int numberB = new Random(). Next(1, 100);
+Console.WriteLine(numberB);
+int max = 0;
+if (numberA > numberB) max = numberA;
+if (numberB > numberA) max = numberB;
+Console.WriteLine (max);
